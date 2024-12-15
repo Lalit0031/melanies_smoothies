56772902d9f8a59a09498a89494954e864236dad
Lalit Kumar <69742783+Lalit0031@users.noreply.github.com>
2024-12-15 22:06:30 +0530

    Update streamlit_app.py
    Added json response

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,8 @@
 import streamlit as st
 # from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
+import requests
+import json
 
 st.title(":cup_with_straw: Customize Your Smoothie!! :cup_with_straw:")
 st.write("""Choose the fruits you want in your custom Smoothie.""")
@@ -39,8 +41,8 @@ if ingredients_list:
         session.sql(my_insert_stmt).collect()
         st.success('Your Smoothie is ordered!', icon="✅")
 
-
-import requests
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-st.text(smoothiefroot_response)
+st.text(smoothiefroot_response.json())
+
+sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
